5d387558bcad1758821ea4b5464d60b27e2c3f1a
Shishkin Andrey <shish27@yandex.ru>
2024-04-17 22:18:52 +0500

    correct eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,16 +4,14 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import pluginJs from "@eslint/js";
+import { URL } from 'node:url'; 
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
+const __filename = new URL('', import.meta.url).pathname;
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
 
 export default [
   {languageOptions: { globals: globals.node }},
   ...compat.extends("airbnb-base"),
-  {
-    ignores: ["eslint.config.js"]
-  }
 ];
--- src/cli.js
+++ src/cli.js
@@ -3,8 +3,7 @@
 /* eslint-disable no-console */
 import readlineSync from 'readline-sync';
 
-const userName = () => {
+export default () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
-export default userName;
